8b8cde251c8a08340c601fc793d7b453c169e61a
Ryan Thompson <service@aiwebsystems.com>
2014-11-05 02:16:49 -0600

    Nav

--- src/DashboardModule.php
+++ src/DashboardModule.php
@@ -4,12 +4,5 @@ use Anomaly\Streams\Platform\Addon\Module\Module;
 
 class DashboardModule extends Module
 {
-    protected $slug = 'dashboard';
 
-    /**
-     * The icon to represent the module.
-     *
-     * @var string
-     */
-    public $icon = '<i class="ion-speedometer"></i>';
 }
