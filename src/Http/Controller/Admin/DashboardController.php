678190c236d3a636dc80a1aba5f831e50ea18edf
Ryan Thompson <service@aiwebsystems.com>
2015-01-24 13:19:27 -0600

    fix resolution

--- src/Http/Controller/Admin/DashboardController.php
+++ src/Http/Controller/Admin/DashboardController.php
@@ -3,15 +3,46 @@
 use Anomaly\DashboardModule\Dashboard\Contract\DashboardRepositoryInterface;
 use Anomaly\Streams\Platform\Http\Controller\AdminController;
 
+/**
+ * Class DashboardController
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\DashboardModule\Http\Controller\Admin
+ */
 class DashboardController extends AdminController
 {
 
-    public function index($dashboard = null, DashboardRepositoryInterface $dashboards)
+    /**
+     * The dashboard repository.
+     *
+     * @var DashboardRepositoryInterface
+     */
+    protected $dashboards;
+
+    /**
+     * Create a new DashboardController instance.
+     *
+     * @param DashboardRepositoryInterface $dashboards
+     */
+    public function __construct(DashboardRepositoryInterface $dashboards)
     {
-        $dashboard = $dashboards->get($dashboard);
+        $this->dashboards = $dashboards;
+    }
+
+    /**
+     * Display the dashboard.
+     *
+     * @param null $dashboard
+     * @return \Illuminate\View\View
+     */
+    public function index($dashboard = null)
+    {
+        $dashboard = $this->dashboards->get($dashboard);
 
         if (!$dashboard) {
-            $dashboard = $dashboards->getDefault();
+            $dashboard = $this->dashboards->getDefault();
         }
 
         return view('anomaly.module.dashboard::admin/index', compact('dashboard'));
