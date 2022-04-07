a7232879028eec0aec831273a70f2eed858020ab
Metehan Celik <mthncelik@gmail.com>
2022-04-07 12:22:06 +0300

    admin panel translations

--- resources/lang/tr/button.php
+++ resources/lang/tr/button.php
@@ -4,4 +4,5 @@ return [
     'new_widget'    => 'Yeni Widget',
     'new_dashboard' => 'Yeni Yönetim Tablosu',
     'manage'        => 'Yönetim Tablolarını Yönet',
+    'find_widgets'  => 'Widget\'ları Bul',
 ];
